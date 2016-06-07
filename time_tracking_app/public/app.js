53d9b86451557ca61fe7c9ad9b22771711ffd421
Grzegorz Pawlik <pawlik.gregor@gmail.com>
2016-06-07 22:18:04 +0200

    Added timer deletion functionality

--- time_tracking_app/public/app.js
+++ time_tracking_app/public/app.js
@@ -25,6 +25,9 @@ const TimersDashboard = React.createClass({
     handleEditFormSubmit: function (attrs) {
         this.updateTimer(attrs);
     },
+    handleDeleteTimer: function (id) {
+        this.deleteTimer(id);
+    },
     createTimer: function (timer) {
         const t = helpers.newTimer(timer);
         this.setState({
@@ -44,6 +47,12 @@ const TimersDashboard = React.createClass({
                 }
             }),
         });
+    },
+    deleteTimer: function (timerId) {
+        console.log('got up?', timerId);
+        this.setState({
+            timers: this.state.timers.filter(timer => timer.id !== timerId),
+        });        
     },    
     render: function () {
         return (
@@ -52,6 +61,7 @@ const TimersDashboard = React.createClass({
                     <EditableTimersList 
                         timers={this.state.timers}
                         onFormSubmit={this.handleEditFormSubmit}
+                        onDeleteTimer={this.handleDeleteTimer}
                         />
                     <ToggleableTimerForm
                         onFormSubmit={this.handleCreateFormSubmit}
@@ -74,6 +84,7 @@ const EditableTimersList = React.createClass({
                     elapsed={timer.elapsed}
                     runningSince={timer.runningSince}
                     onFormSubmit={this.props.onFormSubmit}
+                    onDeleteTimer={this.props.onDeleteTimer}
                 />
             );
         });
@@ -95,6 +106,9 @@ const EditableTimer = React.createClass({
     },    
     handleEditClick: function () {
         this.openForm();
+    },
+    handleDeleteClick: function () {
+        this.props.onDeleteTimer(this.props.id);
     },
     handleFormClose: function () {
         this.closeForm();
@@ -129,6 +143,7 @@ const EditableTimer = React.createClass({
                     elapsed={this.props.elapsed}
                     runningSince={this.props.runningSince}
                     onEditClick={this.handleEditClick}
+                    onDeleteClick={this.handleDeleteClick}
                 />
             );
         }
@@ -235,7 +250,9 @@ const Timer = React.createClass({
                             >                        
                             <i className='edit icon'></i>
                         </span>
-                        <span className='right floated trash icon'>
+                        <span 
+                            className='right floated trash icon'
+                            onClick={this.props.onDeleteClick}>
                             <i className='trash icon'></i>
                         </span>
                     </div>
